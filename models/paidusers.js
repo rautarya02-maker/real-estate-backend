06a1d51ff87133afc7c22cc3dae8f45b87d6b659
rautarya02-maker <rautarya02@gmail.com>
2026-02-16 00:38:38 +0530

    Update paidusers.js

--- models/paidusers.js
+++ models/paidusers.js
@@ -2,43 +2,26 @@ import mongoose from "mongoose";
 
 const PaidUserSchema = new mongoose.Schema(
   {
-    name: {
-      type: String,
-      default: "Direct Paid User"
-    },
-    email: {
-      type: String,
-      default: null
-    },
-    phone: {
-      type: String,
-      default: null
-    },
-
     amount: {
       type: Number,
       required: true
     },
-
-    paymentStatus: {
-      type: String,
-      enum: ["PAID"],
-      default: "PAID"
-    },
-
     paymentId: {
       type: String,
-      required: true
+      required: true,
+      unique: true
     },
-
     orderId: {
       type: String,
       required: true
     },
-
     paymentMethod: {
       type: String,
       default: "UPI"
+    },
+    status: {
+      type: String,
+      default: "PAID"
     }
   },
   { timestamps: true }
